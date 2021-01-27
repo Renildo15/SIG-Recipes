e982bad7b0c0e97db2bc6e088a057b2610b432ad
“Renildo15” <renildorabi@ufrn.edu.br>
2021-01-27 18:04:29 -0300

    criando os submenus

--- SIG_Recipes.c
+++ SIG_Recipes.c
@@ -10,45 +10,49 @@
 #include <stdlib.h>
 
 void menuPrincipal (void);
+void cadastrarReceita (void);
+void consultarReceita (void);
 
 void menuSobre(void);
 int main(void) {
     menuSobre();
     menuPrincipal();
+    cadastrarReceita();
+    consultarReceita();
     
     return 0;
 }
 void menuSobre(void) {
-   system("cls");
-        printf("\n");
-        printf("///////////////////////////////////////////////////////////////////////////////\n");
-        printf("///                                                                         ///\n");
-        printf("///             Universidade Federal do Rio Grande do Norte                 ///\n");
-        printf("///                 Centro de Ensino Superior do Seridó                     ///\n");
-        printf("///               Departamento de Computação e Tecnologia                   ///\n");
-        printf("///                  Disciplina DCT1106 -- Programação                      ///\n");
-        printf("///            Projeto Um Caderno Virtual de Receitas Culinárias            ///\n");
-        printf("///              = == ==       RECIPES       = == ==                        ///\n");
-        printf("///                Developed by  @Renildo Rabi - Jan, 2021                  ///\n");
-        printf("///                                                                         ///\n");
-        printf("///////////////////////////////////////////////////////////////////////////////\n");
-        printf("///                                                                         ///\n");
-        printf("///  = == == SIG-Recipes: Um Caderno Virtual de Receitas Culinárias == == = ///\n");
-        printf("///                                                                         ///\n");
-        printf("///  Fábricas de produtos alimentícios, restaurantes, lanchonetes,          ///\n");
-        printf("///  padarias, ou até mesmo usuários domésticos necessitam de receita       ///\n");
-        printf("///  para conduzir a preparação de determinados alimentos.                  ///\n");
-        printf("///  A principio o objetivo do projeto é adicionar receitas de comidas,     ///\n");
-        printf("///  procurar os tipos de pratos por ingrediente,saber os custos e          ///\n");
-        printf("///  quantidades de ingredientes,a otimização do estoque e a categorização  ///\n");
-        printf("///  das receitas.                                                          ///\n");
-        printf("///                                                                         ///\n");
-        printf("///      Em desenvolvimento pelo aluno Renildo Rabi Vale Dos Santos         ///\n");
-        printf("///                                                                         ///\n");
-        printf("///////////////////////////////////////////////////////////////////////////////\n");
-        printf("\n");
-        printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
-        getchar();
+    system("cls");
+    printf("\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                         ///\n");
+    printf("///             Universidade Federal do Rio Grande do Norte                 ///\n");
+    printf("///                 Centro de Ensino Superior do Seridó                     ///\n");
+    printf("///               Departamento de Computação e Tecnologia                   ///\n");
+    printf("///                  Disciplina DCT1106 -- Programação                      ///\n");
+    printf("///            Projeto Um Caderno Virtual de Receitas Culinárias            ///\n");
+    printf("///              = == ==       RECIPES       = == ==                        ///\n");
+    printf("///                Developed by  @Renildo Rabi - Jan, 2021                  ///\n");
+    printf("///                                                                         ///\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                         ///\n");
+    printf("///  = == == SIG-Recipes: Um Caderno Virtual de Receitas Culinárias == == = ///\n");
+    printf("///                                                                         ///\n");
+    printf("///  Fábricas de produtos alimentícios, restaurantes, lanchonetes,          ///\n");
+    printf("///  padarias, ou até mesmo usuários domésticos necessitam de receita       ///\n");
+    printf("///  para conduzir a preparação de determinados alimentos.                  ///\n");
+    printf("///  A principio o objetivo do projeto é adicionar receitas de comidas,     ///\n");
+    printf("///  procurar os tipos de pratos por ingrediente,saber os custos e          ///\n");
+    printf("///  quantidades de ingredientes,a otimização do estoque e a categorização  ///\n");
+    printf("///  das receitas.                                                          ///\n");
+    printf("///                                                                         ///\n");
+    printf("///      Em desenvolvimento pelo aluno Renildo Rabi Vale Dos Santos         ///\n");
+    printf("///                                                                         ///\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("\n");
+    printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
+    getchar();
 }
 
 void menuPrincipal (void){
@@ -69,12 +73,12 @@ void menuPrincipal (void){
 	printf("///           = = = = = = = =  Menu Principal = = = = = = = =             ///\n");
 	printf("///           = = = = = = = = = = = = = = = = = = = = = = = =             ///\n");
 	printf("///                                                                       ///\n");
-	printf("///           1. Cadastrar receita                                        ///\n");
-	printf("///           2. Consultar receita                                        ///\n");
+	printf("///           1. Cadastrar receitas                                        ///\n");
+	printf("///           2. Consultar receitas                                        ///\n");
 	printf("///           3. Consultar preços                                         ///\n");
 	printf("///           4. Estoque                                                  ///\n");
 	printf("///           5. Quebra de caixa                                          ///\n");
-	printf("///           0. Encerra o programa                                       ///\n");
+	printf("///           0. Encerrar o programa                                       ///\n");
 	printf("///                                                                       ///\n");
 	printf("///           Escolha a opção desejada:                                   ///\n");
 	printf("///                                                                       ///\n");
@@ -83,4 +87,64 @@ void menuPrincipal (void){
 	printf("\n");
     printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
 	getchar();
+}
+
+void cadastrarReceita(void){
+    system("cls");
+    printf("/////////////////////////////////////////////////////////////////////////////\n");
+	printf("///                                                                       ///\n");
+	printf("///          ===================================================          ///\n");
+	printf("///          = = = = = = = = = = = = = = = = = = = = = = = = = =          ///\n");
+	printf("///          = = = =         Receitas Culinárias         = = = =          ///\n");
+	printf("///          = = = = = = = = = = = = = = = = = = = = = = = = = =          ///\n");
+	printf("///          ===================================================          ///\n");
+	printf("///                Developed by  @R.Rabi - Jan, 2021                      ///\n");
+	printf("///                                                                       ///\n");
+	printf("/////////////////////////////////////////////////////////////////////////////\n");
+	printf("///                                                                       ///\n");
+	printf("///           = = = = = = = = = = = = = = = = = = = = = = = =             ///\n");
+	printf("///           = = = = =      Cadastrar Receitas      = = = = =            ///\n");
+	printf("///           = = = = = = = = = = = = = = = = = = = = = = = =             ///\n");
+    printf("///                                                                       ///\n");
+	printf("///             Nome do prato:                                            ///\n");
+	printf("///             Ingredientes:                                             ///\n");
+	printf("///             Preço:                                                    ///\n");
+	printf("///             Categoria:                                                ///\n");
+	printf("///                                                                       ///\n");
+	printf("/////////////////////////////////////////////////////////////////////////////\n");
+	printf("\n");
+    printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
+    getchar();
+}
+
+void consultarReceita() {
+    system("cls");
+     printf("/////////////////////////////////////////////////////////////////////////////\n");
+	printf("///                                                                       ///\n");
+	printf("///          ===================================================          ///\n");
+	printf("///          = = = = = = = = = = = = = = = = = = = = = = = = = =          ///\n");
+	printf("///          = = = =         Receitas Culinárias         = = = =          ///\n");
+	printf("///          = = = = = = = = = = = = = = = = = = = = = = = = = =          ///\n");
+	printf("///          ===================================================          ///\n");
+	printf("///                Developed by  @R.Rabi - Jan, 2021                      ///\n");
+	printf("///                                                                       ///\n");
+	printf("/////////////////////////////////////////////////////////////////////////////\n");
+	printf("///                                                                       ///\n");
+	printf("///           = = = = = = = = = = = = = = = = = = = = = = = =             ///\n");
+	printf("///           = = = = =      Consultar Receitas     = = = = =             ///\n");
+	printf("///           = = = = = = = = = = = = = = = = = = = = = = = =             ///\n");
+    printf("///                                                                       ///\n");
+	printf("///           1. Buscar receita                                           ///\n");
+	printf("///           2. Buscar receitas por categoria                            ///\n");
+	printf("///           3. Buscar receita por ingrediente                           ///\n");
+	printf("///           4.                                                          ///\n");
+    printf("///           0. Encerrar                                                 ///\n");
+    printf("///                                                                       ///\n");
+	printf("///           Escolha a opção desejada:                                   ///\n");
+	printf("///                                                                       ///\n");
+	printf("///                                                                       ///\n");
+	printf("/////////////////////////////////////////////////////////////////////////////\n");
+	printf("\n");
+    printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
+    getchar();
 }
