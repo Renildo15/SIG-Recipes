2a28fd85b325d4a69afac8802c3743e1610b3ca8
“Renildo15” <renildorabi@ufrn.edu.br>
2021-01-28 19:39:00 -0300

    Submenus

--- SIG_Recipes.c
+++ SIG_Recipes.c
@@ -15,6 +15,7 @@ void consultarReceita (void);
 void consultarPreco (void);
 void controleEstoque (void);
 void menuSobre(void);
+void prepararReceita(void);
 
 int main(void) {
     menuSobre();
@@ -23,6 +24,7 @@ int main(void) {
     consultarReceita();
     consultarPreco();
 	controleEstoque();
+	prepararReceita();
 
     
     return 0;
@@ -216,5 +218,35 @@ void controleEstoque() {
     printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
     getchar();
 
+
+}
+
+void prepararReceita() {
+	system("clear");
+	printf("/////////////////////////////////////////////////////////////////////////////\n");
+	printf("///                                                                       ///\n");
+	printf("///          ===================================================          ///\n");
+	printf("///          = = = = = = = = = = = = = = = = = = = = = = = = = =          ///\n");
+	printf("///          = = = =         Receitas Culinárias         = = = =          ///\n");
+	printf("///          = = = = = = = = = = = = = = = = = = = = = = = = = =          ///\n");
+	printf("///          ===================================================          ///\n");
+	printf("///                Developed by  @R.Rabi - Jan, 2021                      ///\n");
+	printf("///                                                                       ///\n");
+	printf("/////////////////////////////////////////////////////////////////////////////\n");
+	printf("///                                                                       ///\n");
+	printf("///           = = = = = = = = = = = = = = = = = = = = = = = =             ///\n");
+	printf("///           = = = = =       Preparar receita      = = = = =             ///\n");
+	printf("///           = = = = = = = = = = = = = = = = = = = = = = = =             ///\n");
+	printf("///                                                                       ///\n");
+	printf("///    Essa opção serve para simular a preparação de uma receita e dizer  ///\n");
+	printf("///    se é possível prepara-lá na vida real baseando-se nos ingredientes ///\n");
+	printf("///    disponivéis no estoque.                                            ///\n");
+	printf("///                                                                       ///\n");
+	printf("///             Buscar nome da receita:                                   ///\n");
+	printf("///                                                                       ///\n");
+	printf("/////////////////////////////////////////////////////////////////////////////\n");
+	printf("\n");
+    printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
+    getchar();
 
 }
